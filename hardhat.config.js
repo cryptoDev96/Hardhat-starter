b714de2c5b3252f17594dae57581d51f6139c18a
cryptoDev96 <cryptoDev96@outlook.com>
2022-05-11 12:09:29 +0900

    Hardhat-Alchemy

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomiclabs/hardhat-waffle");
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "rHD6U5qGMfKy1KpAYbIyxLHa3f8oOIwx";
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = "8af6a89d193c06e75085e4b44969580fe750c402c05aebae6bc05eb408a4ea1a";
+
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
+  
+};
